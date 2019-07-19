47d046ce77b8b6bdb50e8bc67bc5aa6dab628335
Scott Olsen <scg.olsen@gmail.com>
2019-07-19 12:40:44 -0400

    Union Find: Add newlines for clarity

--- src/union_find.rs
+++ src/union_find.rs
@@ -10,15 +10,19 @@ pub fn quick_find(p: usize, q: usize) -> (i32, i32){
   }
   
   let result = loop {
+    
     if id[p] == id[q] {
       break (id[p], id[q]);
     }
+    
     let t: i32 = id[p];
+    
     for n in 0..10000 {
       if id[n] == t {
         id[n] = id[q];
       }  
     }
+  
   };
   
   result
