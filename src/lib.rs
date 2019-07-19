3ac245fc749292b3e47ad4003fa4c800fda90729
Scott Olsen <scg.olsen@gmail.com>
2019-07-19 16:49:28 -0400

    Union Find: Add weighted-quick-union
    Adds a first shot implementation of weighted-quick-union. Like the
    quick-union algorithm before it, we can likely use language constructs
    to avoid while loops and casting. Weighted quick union is identical to
    quick union except in the union step, in which we make a connection on
    the smaller tree.

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,19 @@ mod tests {
       assert_eq!(2 + 2, 4);
   }
 
- // union-find tests
- #[test]
- fn test_quick_find() {
-   assert_eq!((4, 4), union_find::quick_find(3, 4))    
- }
- 
- #[test]
- fn test_quick_union() {
-  assert_eq!((4, 4), union_find::quick_union(3, 4))    
- }
+  // union-find tests
+  #[test]
+  fn test_quick_find() {
+    assert_eq!((4, 4), union_find::quick_find(3, 4))    
+  }
+  
+  #[test]
+  fn test_quick_union() {
+   assert_eq!((4, 4), union_find::quick_union(3, 4))    
+  }
+
+  #[test]
+  fn test_weighted_quick_union() {
+    assert_eq!((3, 3), union_find::weighted_quick_union(3, 4))   
+  }
 }
--- src/union_find.rs
+++ src/union_find.rs
@@ -49,3 +49,37 @@ pub fn quick_union(p: usize, q: usize) -> (i32, i32) {
 
   result
 }
+
+/// The weighted-quick-union algorithm.
+pub fn weighted_quick_union(p: usize, q: usize) -> (i32, i32) {
+  let mut id: Vec<i32> = (0..10000).collect();
+  // The initial size of all trees is one.
+  let mut sizes: Vec<i32> = vec![1; 10000];
+
+  let result = loop {
+    let mut i: usize = p;
+    let mut j: usize = q;
+
+    while  i != id[i] as usize {
+      i = id[i] as usize; 
+    }
+
+    while j != id[j] as usize {
+      j = id[j] as usize;    
+    }
+
+    if i == j {
+      break (id[p], id[q]);     
+    }
+
+    if sizes[i] < sizes[j] {
+      id[i] = j as i32;
+      sizes[j] += sizes[i]; 
+    } else {
+      id[j] = i as i32;
+      sizes[i] += sizes[j];
+    }
+  };
+
+  result
+}
