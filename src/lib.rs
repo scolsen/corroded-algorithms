c8e9df6b1af5772e1f229b4c9d5b002b2a67a14b
Scott Olsen <scg.olsen@gmail.com>
2019-07-19 13:54:54 -0400

    Union Find: Add quick-union algorithm
    Adds a naive implementation of the quick-union algorithm. One can
    already sense the approach is not an ideal means of expressing this
    algorithm in rust. We make several type-casts to ensure we can index
    into the vector:
    
    ```rust
    while i != id[i] as usize {
          i = id[i] as usize;
        }
    
    while j != id[j] as usize {
      j = id[j] as usize;
    }
    ```
    
    There's likely a cleaner way to do this.

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,14 @@ mod tests {
       assert_eq!(2 + 2, 4);
   }
 
- // union-find
+ // union-find tests
  #[test]
  fn test_quick_find() {
    assert_eq!((4, 4), union_find::quick_find(3, 4))    
+ }
+ 
+ #[test]
+ fn test_quick_union() {
+  assert_eq!((4, 4), union_find::quick_union(3, 4))    
  }
 }
--- src/union_find.rs
+++ src/union_find.rs
@@ -1,4 +1,5 @@
 // union-find algorithms
+
 /// The quick-find algorithm.
 pub fn quick_find(p: usize, q: usize) -> (i32, i32){
   // Initialize a vector of integers of a fixed size.
@@ -20,5 +21,31 @@ pub fn quick_find(p: usize, q: usize) -> (i32, i32){
   
   };
   
+  result
+}
+
+/// The quick-union algorithm.
+pub fn quick_union(p: usize, q: usize) -> (i32, i32) {
+  let mut id: Vec<i32> = (0..10000).collect();
+
+  let result = loop {
+    let mut i: usize = p;
+    let mut j: usize = q;
+    
+    while i != id[i] as usize {
+      i = id[i] as usize; 
+    }
+    
+    while j != id[j] as usize {
+      j = id[j] as usize;     
+    }
+
+    if i == j {
+      break (id[p], id[q]);     
+    }
+
+    id[i] = j as i32;
+  };
+
   result
 }
